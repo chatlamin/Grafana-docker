080023cffd16e17dd66fb59432988c30c5ddfcbc
chatlamin <viktorap@inbox.ru>
2019-06-21 15:19:23 +0300

    config

--- grafana.sh
+++ grafana.sh
@@ -6,5 +6,6 @@ docker run --name grafana \
   --volume /etc/localtime:/etc/localtime:ro \
   --volume /etc/timezone:/etc/timezone:ro \
   --volume /home/docker/containers/grafana/var/lib/grafana:/var/lib/grafana \
+  --volume /home/docker/containers/grafana/etc/grafana/grafana.ini:/etc/grafana/grafana.ini \
   --publish 3000:3000 \
   grafana/grafana
